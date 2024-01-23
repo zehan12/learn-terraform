6af5302a8fc15b64faf21ccddc60c60c6b352cd5
Zehan Khan <zkhan@hootboard.com>
2024-01-23 20:55:25 +0530

    create multiple terraform block

---
+++ multiple.tf
@@ -0,0 +1,11 @@
+output "firstoutputblock" {
+	value = "this is first block"
+}
+
+output "secondoutputblock" {
+	value = "this is  second block"
+}
+
+output "thirdoutputblock" {
+	value = "this is third block"
+}
